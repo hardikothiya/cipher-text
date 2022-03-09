f81b8b08615ae6b2ba8ac1ed77ee02dbb762e5aa
hardik kothiya <91317366+hardikothiya@users.noreply.github.com>
2022-03-09 11:40:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-direction = str(input("Type 'encode' to encrypt, type 'decode' to decrypt:\n"))
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-shift = shift % 25
-
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
 def caesar(p_text, shift_amount, sl_direction):
-    
-    
- text = ''
- for letter in p_text:
-   if letter in alphabet:
-     position = alphabet.index(letter)
-     if sl_direction == 'encode':
-      new_position = position + shift_amount
+    text = ''
+    for letter in p_text:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            if sl_direction == 'encode':
+                new_position = position + shift_amount
 
-     elif sl_direction == 'decode':
-       new_position = position - shift_amount
-       
-     new_letter = alphabet[new_position]
-     text += new_letter
+            elif sl_direction == 'decode':
+                new_position = position - shift_amount
+
+            new_letter = alphabet[new_position]
+            text += new_letter
 
 
-   else :
-     text += letter
+        else:
+            text += letter
 
-    
-   
+    print(text)
 
- print(text)
 
-caesar(p_text = text, shift_amount = shift, sl_direction = direction)
+should_end = False
+while not should_end:
+    direction = str(input("Type 'encode' to encrypt, type 'decode' to decrypt:\n"))
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 25
+    run_again = 'true'
+
+    caesar(p_text=text, shift_amount=shift, sl_direction=direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
